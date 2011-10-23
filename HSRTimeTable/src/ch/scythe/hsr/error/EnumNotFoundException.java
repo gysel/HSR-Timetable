9800ee9c3503f11e2633f409a0e859246aabab07
Michi Gysel <michi@scythe.ch>
2011-10-23 21:46:30 +0200

    fixed eclipse warning

--- HSRTimeTable/src/ch/scythe/hsr/error/EnumNotFoundException.java
+++ HSRTimeTable/src/ch/scythe/hsr/error/EnumNotFoundException.java
@@ -2,6 +2,8 @@ package ch.scythe.hsr.error;
 
 public class EnumNotFoundException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 	public EnumNotFoundException(String detailMessage) {
 		super(detailMessage);
 	}
