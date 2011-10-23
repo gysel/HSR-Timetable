3297a973669247f4d3c6eb603f0ef51351810bde
Michi Gysel <michi@scythe.ch>
2011-10-23 21:46:01 +0200

    refactored sax parser to use new api

--- HSRTimeTable/src/ch/scythe/hsr/xml/LessonHandler.java
+++ HSRTimeTable/src/ch/scythe/hsr/xml/LessonHandler.java
@@ -3,14 +3,14 @@ package ch.scythe.hsr.xml;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.xml.sax.AttributeList;
-import org.xml.sax.HandlerBase;
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 import ch.scythe.hsr.entity.Lesson;
 import ch.scythe.hsr.error.EnumNotFoundException;
 
-public class LessonHandler extends HandlerBase {
+public class LessonHandler extends DefaultHandler {
 
 	private static final String XML_NODE_ROOM = "Room";
 	private static final String XML_ATTRIBUTE_ID = "id";
@@ -40,20 +40,19 @@ public class LessonHandler extends HandlerBase {
 	}
 
 	@Override
-	public void startElement(String name, AttributeList attributes) throws SAXException {
-
-		super.startElement(name, attributes);
-		if (name.equalsIgnoreCase(XML_NODE_LESSON)) {
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		super.startElement(uri, localName, qName, attributes);
+		if (qName.equalsIgnoreCase(XML_NODE_LESSON)) {
 			currentLesson = new Lesson();
 			currentLesson.setIdentifier(attributes.getValue(XML_ATTRIBUTE_ID));
-		} else if (currentLesson != null && name.equalsIgnoreCase(XML_NODE_TIME_UNIT)) {
+		} else if (currentLesson != null && qName.equalsIgnoreCase(XML_NODE_TIME_UNIT)) {
 			try {
 				currentLesson.addTimeUnit(Integer.parseInt(attributes.getValue(XML_ATTRIBUTE_ID)));
 			} catch (EnumNotFoundException e) {
 				throw new SAXException(e);
 			}
 
-		} else if (currentLesson != null && name.equalsIgnoreCase(XML_NODE_ROOM)) {
+		} else if (currentLesson != null && qName.equalsIgnoreCase(XML_NODE_ROOM)) {
 			// TODO one lesson can have several rooms.
 			currentLesson.setRoom(attributes.getValue(XML_ATTRIBUTE_ID));
 		}
@@ -61,14 +60,14 @@ public class LessonHandler extends HandlerBase {
 	}
 
 	@Override
-	public void endElement(String name) throws SAXException {
-		super.endElement(name);
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		super.endElement(uri, localName, qName);
 		if (this.currentLesson != null) {
-			if (name.equalsIgnoreCase(XML_NODE_TYPE)) {
+			if (qName.equalsIgnoreCase(XML_NODE_TYPE)) {
 				currentLesson.setType(builder.toString().trim());
-			} else if (name.equalsIgnoreCase(XML_NODE_NAME_SHORT)) {
+			} else if (qName.equalsIgnoreCase(XML_NODE_NAME_SHORT)) {
 				currentLesson.addLecturer(builder.toString().trim());
-			} else if (name.equalsIgnoreCase(XML_NODE_LESSON)) {
+			} else if (qName.equalsIgnoreCase(XML_NODE_LESSON)) {
 				lessons.add(currentLesson);
 			}
 			builder.setLength(0);
