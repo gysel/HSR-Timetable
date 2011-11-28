c525919f21c299cdcfa2e94c64b93f2b9821d914
Michi Gysel <michi@scythe.ch>
2011-11-28 17:01:57 +0100

    fixed test in case locale was not german

--- HSRTimeTableTest/src/ch/scythe/hsr/helper/DateHelperTest.java
+++ HSRTimeTableTest/src/ch/scythe/hsr/helper/DateHelperTest.java
@@ -3,6 +3,7 @@ package ch.scythe.hsr.helper;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Date;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -10,7 +11,12 @@ public class DateHelperTest {
 
 	@Test
 	public void testFormatToUserFriendlyFormat() {
+		Locale valueBefore = Locale.getDefault();
+		//
+		Locale.setDefault(Locale.GERMAN);
 		assertEquals("Mo 31.10.11", DateHelper.formatToUserFriendlyFormat(new Date(2011 - 1900, 10 - 1, 31)));
+		//
+		Locale.setDefault(valueBefore);
 	}
 
 }
