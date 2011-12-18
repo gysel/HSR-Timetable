10ad43b0276ec8b062e88248253c8d8b087ae129
Michi Gysel <michi@scythe.ch>
2011-12-19 00:12:39 +0100

    added gpl v3 headers and license file

--- HSRTimeTable/src/ch/scythe/hsr/xml/TimeTableWeekHandler.java
+++ HSRTimeTable/src/ch/scythe/hsr/xml/TimeTableWeekHandler.java
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2011 Michi Gysel <michael.gysel@gmail.com>
+ *
+ * This file is part of the HSR Timetable.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ch.scythe.hsr.xml;
 
 import java.util.ArrayList;
